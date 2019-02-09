5989aa495a6ac204d1521bc56cd22dc91cb7dc23
MelvinLaubstein <mdlaubstein@oakland.edu>
2019-02-09 18:17:26 -0500

    Main method code created.

--- BeerSong.java
+++ BeerSong.java
@@ -1,3 +1,8 @@
-Main()
+public class BeerSong {
+    public static void main(String[] args) {
+        Ninety_Nine_Bottles_of_Beer();
+    }
+	
+	public static void Ninety_Nine_Bottles_of_Beer() {
 
-Ninety_Nine_Bottles_of_Beer()
+}
